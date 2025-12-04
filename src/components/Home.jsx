65882568b15f5cc3f35973634c1fe73971c2ab40
VICTETOR-DEV <victetor.dev@gmail.com>
2025-12-03 22:05:15 -0800

    added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,14 @@ import '../styles/home.css'
 
 const Home = () => {
   return (
-    <div className='home' >
+    <div className="home">
       <div className="home-content">
         <h1>
           <span>Connecting</span> Ideas Through <br /> <span>Technology</span>
         </h1>
         <div className="home-buttons">
-          <button>Learn More</button>
-          <button className="active">Get Started</button>
+          <button className="button-two style-2" data-text="Learn More" >Learn More</button>
+          <button className="button-two style-2 active"  data-text="Get Started">Get Started</button>
         </div>
       </div>
     </div>
